c73a77b21d9fbebfa5f81632b092d4058d56770d
Erik Erlandson <eerlands@redhat.com>
2011-08-11 09:55:48 -0700

    cmd_scale_test.rb -- scale testing harness

---
+++ wallaby/cmd_scale_test.rb
@@ -0,0 +1,79 @@
+# cmd_scale_test.rb:  condor scale test
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+  
+$LOAD_PATH << "#{ENV['WALLABY_COMMAND_DIR']}"
+require 'albatross_wallaby_tools.rb'
+
+
+module Mrg
+  module Grid
+    module Config
+      module Shell
+        class ScaleTest < ::Mrg::Grid::Config::Shell::Command
+          # opname returns the operation name; for "wallaby foo", it
+          # would return "foo".
+          def self.opname
+            "scale-test"
+          end
+        
+          # description returns a short description of this command, suitable 
+          # for use in the output of "wallaby help commands".
+          def self.description
+            "condor scale test"
+          end
+        
+          def init_option_parser
+            # Edit this method to generate a method that parses your command-line options.
+            OptionParser.new do |opts|
+              @params = {}
+
+              opts.banner = "Usage:  wallaby #{self.class.opname}\n#{self.class.description}"
+        
+              opts.on("-h", "--help", "displays this message") do
+                puts @oparser
+                exit
+              end
+            end
+          end
+
+          class ScaleTest < ::Test::Unit::TestCase
+            include ::Albatross::WallabyTools
+            include ::Albatross::WallabyUnitTestTools
+
+            def suite_setup
+            end
+
+            def suite_teardown
+            end
+
+            def test_commit
+            end
+          end
+        
+          def act
+            ScaleTest.store=(store)
+            ScaleTest.params=(@params)
+
+            ::Test::Unit::UI::Console::TestRunner.run(ScaleTest)
+
+            return 0
+          end
+        end
+      end
+    end
+  end
+end
